b559fa2ebd908cd5a278f9a7fe8c494244ca9d7f
RamWebDev <ramprasad.web.developer@gmail.com>
2015-12-01 15:30:31 +0530

    Added new msg bix

--- HelloWorldSoln/HelloWorld/Form1.cs
+++ HelloWorldSoln/HelloWorld/Form1.cs
@@ -19,6 +19,7 @@ namespace HelloWorld
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Hello Everyone!!!!");
+            MessageBox.Show("Bye Everyone!!!!");
         }
     }
 }
